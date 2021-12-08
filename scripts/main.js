19664951447f9233f0a34b2506f0ea6855835e0b
Thomas <malifarge.thomas9@gmail.com>
2021-12-08 11:57:33 +0100

    changement du menu avec menu burger sur tel + pub de l'accueil

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+function showResponsiveMenu() {
+    var menu = document.getElementById("navburger");
+    var icon = document.getElementById("burger");
+    var root = document.getElementById("root");
+    if (menu.className === "") {
+        menu.className = "open";
+        icon.className = "open";
+        root.style.overflowY = "hidden";
+    } else {
+        menu.className = "";                    
+        icon.className = "";
+        root.style.overflowY = "";
+    }
+}
