4dd4f971f5b3d4a7ce27f79f5d641b4e3ab51430
Thomas <malifarge.thomas9@gmail.com>
2021-12-08 17:14:18 +0100

    rajout carousel

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+// menu burger
+
 function showResponsiveMenu() {
     var menu = document.getElementById("navburger");
     var icon = document.getElementById("burger");
@@ -12,3 +14,28 @@ function showResponsiveMenu() {
         root.style.overflowY = "";
     }
 }
+
+// carousel
+
+const items = document.querySelectorAll('.slider img')
+const nbSlide = items.length;
+const suivant = document.querySelector('.right')
+const precedent = document.querySelector('.left')
+let count = 1
+
+function slideSuivante(){
+   
+    items[count].classList.remove('active')    
+
+    if(count > 0){
+        count--
+    }else {
+        count = nbSlide - 1
+    }
+    items[count].classList.add('active')
+    console.log(count);
+
+    setTimeout ("slideSuivante()", 4000)
+}
+
+window.onload = slideSuivante
